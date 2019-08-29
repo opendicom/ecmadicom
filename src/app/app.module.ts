9662b694c2d83d256a0e033e60fbbe2ee778c199
Claudio Baeza <cl.baeza@gmail.com>
2019-08-29 17:21:13 -0400

    Add viewer component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { AngularMaterialModule } from './angular-material/angular-material.module'
+import { AngularMaterialModule } from './angular-material/angular-material.module';
+import { ViewerComponent } from './viewer/viewer.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ViewerComponent
   ],
   imports: [
     BrowserModule,
